4eedcc523c16d856bd29699ea84943012da2b9a5
343dev <343dev@users.noreply.github.com>
2020-07-15 19:47:24 +0500

    Move prepareExtensions fn into lib/utils.js

--- cli.js
+++ cli.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
+
 const markdownLint = require('.');
-const prepareExtensions = require('./lib/prepare-extensions');
+const { prepareExtensions } = require('./lib/utils');
 
 program
   .option('--fix', 'Automatically fix problems')
-  .option('--ext <value>', 'Set file extension, e.g. "apib". "md" by default', prepareExtensions, ['md', 'MD'])
+  .option('--ext <value>', 'Specify file extensions', prepareExtensions, ['md', 'MD'])
   .option('-t, --typograph', 'Enable typograph')
   .option('-r, --recursive', 'Get files from provided directory and the entire subtree')
   .option('-c, --config <file>', 'Use this configuration, overriding default options if present');
